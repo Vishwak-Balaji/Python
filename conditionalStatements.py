cfe51fcb78c5615e0829d6a6b4de024cdd612929
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-06-15 11:59:44 +0530

    basic if elif and else

---
+++ conditionalStatements.py
@@ -0,0 +1,15 @@
+# basics (if, elif , else)
+
+grade =95
+
+if grade>=90 and grade <=100:
+    print("Grade - A+")
+elif grade >= 80 and grade <90:
+    print("Grade - A")
+elif grade >= 60 and grade <80:
+    print("Grade - B")
+elif grade >= 50 and grade <60:
+    print("Grade - C")
+else:
+    print("Fail")
+
