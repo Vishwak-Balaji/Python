f387086d0a99f04231e9ea90698510c092f888fe
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-06-15 12:13:35 +0530

    example of nested if

--- conditionalStatements.py
+++ conditionalStatements.py
@@ -1,15 +1,29 @@
 # basics (if, elif , else)
 
-grade =95
+# grade =95
+#
+# if grade>=90 and grade <=100:
+#     print("Grade - A+")
+# elif grade >= 80 and grade <90:
+#     print("Grade - A")
+# elif grade >= 60 and grade <80:
+#     print("Grade - B")
+# elif grade >= 50 and grade <60:
+#     print("Grade - C")
+# else:
+#     print("Fail")
 
-if grade>=90 and grade <=100:
-    print("Grade - A+")
-elif grade >= 80 and grade <90:
-    print("Grade - A")
-elif grade >= 60 and grade <80:
-    print("Grade - B")
-elif grade >= 50 and grade <60:
-    print("Grade - C")
+#==========================================================================================================
+
+# nested if
+
+age = int(input("Enter your age :"))
+if(age >=18):
+    haveLisence = input("Have lisence ? (yes/no) :").lower()
+if(age >= 18):
+    if(haveLisence == "yes"):
+        print("Yes you can rent a bike")
+    else:
+        print("you can't rent a bike")
 else:
-    print("Fail")
-
+    print("you are not eligible to rent a bike")
